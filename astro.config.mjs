fedd6f8009b0483ab093aa6946f90fb88f2499c6
madiyar <madiyar.bolatov97@gmail.com>
2023-09-06 00:08:55 +0600

    decay cms

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,44 @@
+import dayjs from 'dayjs';
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import mdx from "@astrojs/mdx";
+import NetlifyCMS from 'astro-netlify-cms';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), mdx()]
+  integrations: [
+    tailwind(),
+    react(),
+    mdx(),
+    NetlifyCMS({
+      config: {
+        backend: {
+          name: 'git-gateway',
+          branch: 'main',
+        },
+        collections: [
+          // Define a blog post collection
+          {
+            name: 'blog',
+            label: 'Мақалалар',
+            folder: 'src/content/blog',
+            create: true,
+            delete: true,
+            slug: '{{slug}}',
+            fields: [
+              { name: 'title', widget: 'string', label: 'Title' },
+              { name: 'cover', widget: 'image', label: 'Постер', },
+              { name: "pubDate", widget: 'hidden', label: 'Publish Date', default: dayjs().format('YYYY-MM-DD') },
+              { name: 'body', widget: 'markdown', label: 'Post Body' },
+            ],
+          },
+          // Content collections
+        ],
+        previewStyles: [
+          'https://fonts.googleapis.com/css2?family=Geologica:wght@300;400;500;600;700&display=swap',
+        ],
+      },
+    }),
+  ]
 });

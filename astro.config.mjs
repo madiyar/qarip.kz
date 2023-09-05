e47e79826c11105ecdad1900367bb4ad94db8493
madiyar <madiyar.bolatov97@gmail.com>
2023-09-06 00:44:05 +0600

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-// import dayjs from 'dayjs';
+import dayjs from 'dayjs';
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
@@ -17,6 +17,9 @@ export default defineConfig({
           name: 'git-gateway',
           branch: 'main',
         },
+        publish_mode: 'editorial_workflow',
+        media_folder: "src/assets",
+        public_folder: "/_astro",
         collections: [
           // Define a blog post collection
           {
@@ -29,7 +32,7 @@ export default defineConfig({
             fields: [
               { name: 'title', widget: 'string', label: 'Title' },
               { name: 'cover', widget: 'image', label: 'Постер', },
-              // { name: "pubDate", widget: 'hidden', label: 'Publish Date', default: dayjs().format('YYYY-MM-DD') },
+              { name: "pubDate", widget: 'hidden', label: 'Publish Date', default: dayjs().format('YYYY-MM-DD') },
               { name: 'body', widget: 'markdown', label: 'Post Body' },
             ],
           },

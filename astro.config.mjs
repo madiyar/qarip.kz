25988141a0c9dd42c0e821c8a547e3f819171b85
madiyar <madiyar.bolatov97@gmail.com>
2023-09-06 00:18:04 +0600

    check

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import dayjs from 'dayjs';
+// import dayjs from 'dayjs';
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
@@ -29,7 +29,7 @@ export default defineConfig({
             fields: [
               { name: 'title', widget: 'string', label: 'Title' },
               { name: 'cover', widget: 'image', label: 'Постер', },
-              { name: "pubDate", widget: 'hidden', label: 'Publish Date', default: dayjs().format('YYYY-MM-DD') },
+              // { name: "pubDate", widget: 'hidden', label: 'Publish Date', default: dayjs().format('YYYY-MM-DD') },
               { name: 'body', widget: 'markdown', label: 'Post Body' },
             ],
           },

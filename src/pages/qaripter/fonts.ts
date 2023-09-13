69166869f176d5f384ab524350e595af3054b913
madiyar <madiyar.bolatov97@gmail.com>
2023-09-14 00:02:03 +0600

    test3

--- src/pages/qaripter/fonts.ts
+++ src/pages/qaripter/fonts.ts
@@ -1,25 +1,26 @@
 export const prerender = false;
-import queryString from 'query-string';
 import type { APIRoute } from 'astro';
-import { getCollection } from 'astro:content';
+// import queryString from 'query-string';
+// import { getCollection } from 'astro:content';
 
-export const GET: APIRoute = async ({ request }) => {
-  const { query } = queryString.parseUrl(request.url, { arrayFormat: 'comma' });
+export const GET: APIRoute = async () => {
+  // const { query } = queryString.parseUrl(request.url, { arrayFormat: 'comma' });
 
-  const fonts = (await getCollection('qaripter')).sort(
-    (a, b) => a.data.pubDate.valueOf() - b.data.pubDate.valueOf()
-  ).map(({ body, ...item}) => item);
+  // const fonts = (await getCollection('qaripter')).sort(
+  //   (a, b) => a.data.pubDate.valueOf() - b.data.pubDate.valueOf()
+  // ).map(({ body, ...item}) => item);
 
-  const tags = (Array.isArray(query?.tags) ? query?.tags : [query?.tags]).filter(Boolean);
+  // const tags = (Array.isArray(query?.tags) ? query?.tags : [query?.tags]).filter(Boolean);
 
-  const data = fonts.filter(font => {
-    if (Array.isArray(tags) && tags.length) {
-      return font.data.tags.some(tag => tags.includes(tag));
-    }
-    return true;
-  });
+  // const data = fonts.filter(font => {
+  //   if (Array.isArray(tags) && tags.length) {
+  //     return font.data.tags.some(tag => tags.includes(tag));
+  //   }
+  //   return true;
+  // });
 
   return {
-    body: JSON.stringify({ query: { tags }, data }, null, 2),
+    // body: JSON.stringify({ query: { tags }, data }),
+    body: JSON.stringify({}),
   };
 }

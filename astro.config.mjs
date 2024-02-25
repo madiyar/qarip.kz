9e17e36678a664473932069788b8b9d23c68cba2
madiyar <madiyar.bolatov97@gmail.com>
2024-02-25 21:54:28 +0500

    feat: upgrade astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,103 +1,19 @@
-import dayjs from 'dayjs';
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
-import NetlifyCMS from 'astro-netlify-cms';
-
-// import netlify from "@astrojs/netlify/functions";
-// (property) NetlifyCMSOptions.config: Omit<CmsConfig, "load_config_file" | "local_backend">
-const netlifyCMSConfig = {
-  backend: {
-    name: 'git-gateway',
-    branch: 'main'
-  },
-  publish_mode: 'editorial_workflow',
-  media_folder: "src/assets",
-  public_folder: "/src/assets",
-  collections: [
-  // Define a blog post collection
-  {
-    name: 'blog',
-    label: 'Мақала',
-    identifier_field: 'url',
-    folder: 'src/content/blog',
-    create: true,
-    delete: true,
-    slug: '{{slug}}',
-    fields: [{
-      name: 'title',
-      widget: 'string',
-      label: 'Title'
-    }, {
-      name: 'url',
-      widget: 'string',
-      label: 'URL'
-    }, {
-      name: "pubDate",
-      widget: 'datetime',
-      label: 'Publish Date',
-      default: `${dayjs().format('YYYY-MM-DD')}`
-    }, {
-      name: 'author',
-      widget: 'string',
-      label: 'Авторы',
-      required: false
-    }, {
-      name: 'cover',
-      widget: 'image',
-      label: 'Постер'
-    }, {
-      name: 'body',
-      widget: 'markdown',
-      label: 'Post Body'
-    }]
-  }, {
-    name: 'typography',
-    label: 'Типография',
-    folder: 'src/content/typography',
-    create: false,
-    delete: false,
-    slug: '{{slug}}',
-    extension: 'json',
-    fields: [{
-      name: 'words',
-      widget: 'list',
-      label: 'Сөздер',
-      label_singular: 'сөз',
-      fields: [{
-        name: 'title',
-        widget: 'string',
-        label: 'Title'
-      }, {
-        name: 'description',
-        widget: 'text',
-        label: 'Description'
-      }]
-    }]
-  }
-  // Content collections
-  ],
-
-  previewStyles: ['https://fonts.googleapis.com/css2?family=Geologica:wght@300;400;500;600;700&display=swap', ['* { font-family: Geologica, sans-serif }', {
-    raw: true
-  }], ['img { width: 100% }', {
-    raw: true
-  }]]
-}
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://qarip.kz',
   output: 'hybrid',
-  // adapter: netlify(),
+  adapter: netlify(),
   // output: 'hybrid',
   // vite: {
   //   ssr: {
   //     noExternal: ['path-to-regexp'],
   //   },
   // },
-  integrations: [tailwind(), react(), mdx(), NetlifyCMS({
-    config: netlifyCMSConfig
-  })]
+  integrations: [tailwind(), react(), mdx()]
 });
